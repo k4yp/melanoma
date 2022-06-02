457d820685a8b32ea95c2ede25b3bc4797aacb56
keyp0s <aidentubmann@gmail.com>
2022-06-02 17:49:19 +1000

    updates

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ conv_base = ResNet50(weights='imagenet',
                      include_top=False,
                      input_shape=(224, 224, 3))
 
+
 model = models.Sequential()
 model.add(conv_base)
 model.add(layers.Flatten())
@@ -58,4 +59,42 @@ model.add(layers.Dense(64, activation='relu',kernel_regularizer=regularizers.l2(
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(2, activation='sigmoid'))
 
-print(model.summary())
+
+model.compile(optimizer=optimizers.Adam(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+
+batch_size = 20
+target_size = (224, 224)
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   vertical_flip=True,
+                                   fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_gen = train_datagen.flow_from_directory(PATH_TRAIN,
+                                              target_size=target_size,
+                                              batch_size=batch_size)
+
+valid_gen = test_datagen.flow_from_directory(PATH_VALID,
+                                             target_size=target_size,
+                                             batch_size=batch_size)
+
+test_gen = test_datagen.flow_from_directory(PATH_TEST,
+                                            target_size=target_size,
+                                            batch_size=batch_size)
+
+
+history = model.fit_generator(train_gen,
+                              epochs=3,
+                              steps_per_epoch = 10682 // batch_size,
+                              validation_data = valid_gen,
+                              validation_steps = 3562 // batch_size)
