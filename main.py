e606a19ef1e66cc4bf573efd1480c56f653d7035
keyp0s <aidentubmann@gmail.com>
2022-07-26 16:56:37 +1000

    added latex file

--- main.py
+++ main.py
@@ -1,54 +1,63 @@
-import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from keras.utils import load_img
+from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
+
 from keras import models, regularizers, layers, optimizers, losses, metrics
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications import ResNet50
+from keras.preprocessing import image
+from keras.models import load_model
+
+import os
 
 
-PATH = "C:\data\dermmel"
+PATH = "../input/dermmel/DermMel/"
 
-PATH_TRAIN = PATH + '/train/'
+PATH_TRAIN = PATH + '/train_sep/'
 PATH_VALID = PATH + '/valid/'
 PATH_TEST = PATH + '/test/'
 
-def dataset_count(path, type):
+
+#checks the dataset for image counts
+def dataset_check(path, type):
     labels = os.listdir(path)
     total = 0
-    print(type, "photos", total)
     for label in labels:
         print(label,len(os.listdir(path + label)))
         total += len(os.listdir(path + label))
+
+    print(type,"photos",total)
     print ("\n")
 
-dataset_count(PATH_TRAIN, "train")
-dataset_count(PATH_VALID, "validation")
-dataset_count(PATH_TEST, "testing")
+dataset_check(PATH_TRAIN,"training")
+dataset_check(PATH_VALID,"validation")
+dataset_check(PATH_TEST,"testing")
 
-
-def dataset_display(path, sample, cat):
-    image_dir = path + '/' + cat + '/'
-    img_name = os.listdir(image_dir)[sample]
-    img_path = image_dir + img_name
-    img = load_img(img_path, target_size=(224, 224))
+#displays sample image
+def dataset_display(path, sample, type):
+    img_path = path + '/' + type + '/'
+    img_name = os.listdir(img_path)[sample]
+    img_path_full = img_path + img_name
+    img = load_img(img_path_full, target_size=(252, 252))
     imgplot = plt.imshow(img)
-    print(path, "photo number", sample)
+    plt.title(type)
     plt.show()
+    return img_path_full
 
-dataset_display(PATH_TRAIN, 77, 'melanoma')
-dataset_display(PATH_VALID, 77, 'melanoma')
-dataset_display(PATH_TEST, 77, 'melanoma')
+sample_num = 77
 
+print(dataset_display(PATH_TRAIN, sample_num, 'Melanoma'),'sample:',sample_num,'\n')
+print(dataset_display(PATH_TRAIN, sample_num, 'NotMelanoma'),'sample:',sample_num,'\n')
+print(dataset_display(PATH_TEST, sample_num, 'Melanoma'),'sample:',sample_num,'\n')
+print(dataset_display(PATH_TEST, sample_num, 'NotMelanoma'),'sample:',sample_num,'\n')
 
 conv_base = ResNet50(weights='imagenet',
                      include_top=False,
                      input_shape=(224, 224, 3))
-
 
 model = models.Sequential()
 model.add(conv_base)
@@ -58,13 +67,18 @@ model.add(layers.Dense(64, activation='relu',kernel_regularizer=regularizers.l2(
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(2, activation='sigmoid'))
 
+print(model.summary())
+
+for layer in conv_base.layers[:]:
+    layer.trainable = False
 
 model.compile(optimizer=optimizers.Adam(),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+print(model.summary())
 
-batch_size = 32
+batch_size = 20
 target_size = (224, 224)
 
 train_datagen = ImageDataGenerator(rescale=1./255,
@@ -93,10 +107,26 @@ test_gen = test_datagen.flow_from_directory(PATH_TEST,
 
 
 history = model.fit_generator(train_gen,
-                              epochs=1,
+                              epochs=2,
                               steps_per_epoch = 4096 // batch_size,
                               validation_data = valid_gen,
                               validation_steps = 2048 // batch_size)
+
+for layer in conv_base.layers[:165]:
+    layer.trainable = False
+for layer in conv_base.layers[165:]:
+    layer.trainable = True
+
+model.compile(optimizer=optimizers.Adam(lr=1e-5),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit_generator(train_gen,
+                              epochs=32,
+                              steps_per_epoch = 4096 // batch_size,
+                              validation_data = valid_gen,
+                              validation_steps = 2048 // batch_size)
+
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
@@ -114,8 +144,44 @@ plt.title('Training and validation loss')
 plt.legend()
 plt.show()
 
-test_loss, test_acc = model.evaluate_generator(test_gen, steps = 2048 // batch_size, verbose=1)
-print('test acc:', test_acc)
+def display_results(img_num, check_type):
+    def load_image(img_path_full, show = False):
+        img = image.load_img(img_path_full, target_size = (224, 224))
+        img_tensor = image.img_to_array(img)
+        img_tensor = np.expand_dims(img_tensor, axis = 0)
+        img_tensor /= 255
+        
+        return img_tensor
+    
+    PROOF_PATH = dataset_display(PATH_TEST, img_num, check_type)
+    pred = model.predict(load_image(PROOF_PATH))
+    pred = round(float(pred[0][0]),4)*100
+    actual = 100 if check_type == 'Melanoma' else 0
+    diff = abs(round(pred-actual,4))
+    
+    y = ['Predicted','Actual','Accuracy']
+    x = [pred+1,actual+1,(100-diff)]
+    
+    f = plt.figure()
+    f.set_figwidth(3.4)
+    f.set_figheight(1)
+    plt.title('chance of melanoma')
+    plt.barh(y,x,color=['white', 'lightgrey','b' if 100-diff > 76 else 'r'],edgecolor='black')
+    plt.xlim([0,100])
+    plt.show()
+    
+    print (f'Predicted chance of melanoma: {pred}%')
+    print (f"Actual: {actual}%")
+    print (f'Difference: {diff}%')
+    print('\n')
 
-model.save('MelanomaResNet50FineTune.h5')
-print("MelanomaResNet50FineTune.h5 was saved")
+for i in range(4,8):
+    display_results(i,'NotMelanoma')
+for i in range(4,8):
+    display_results(i,'Melanoma')
+
+test_loss, test_acc = model.evaluate_generator(test_gen, steps = 2048 // batch_size, verbose=1)
+print('test accuracy:', test_acc)
+print('test loss:', test_loss)
+
+model.save('melanoma_resnet50.h5')
